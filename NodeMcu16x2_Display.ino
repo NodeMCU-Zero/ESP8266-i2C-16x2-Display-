1b46b8a83f53656517606e46f6c00ff0b5b041fa
Niwantha <niwantha33@gmail.com>
2016-11-15 20:38:06 +0530

    Update and rename ESP8266_16x2_Display.ino to NodeMcu16x2_Display.ino

--- NodeMcu16x2_Display.ino
+++ NodeMcu16x2_Display.ino
@@ -1,4 +1,11 @@
 /*
+Algobel Technologies & Innovations 
+Auth : Niwantha 
+Free to use 
+Original Nodemcu Dev Board 
+
+BuY : http://store.algobel.com
+
 NodeMCU Ver.1 i2C 16x2 LCD SCREEN
 Tested with original AI NodeMcu (D1--> SDL, D2-->SDA)
 LCD 16x2 power supply to be connected to 5V ( Don't connect to 3.3V)
@@ -10,14 +17,16 @@ Common Gorund (5V and NodeMcu)
 
 #include <Wire.h>  // This library is already built in to the Arduino IDE
 #include <LiquidCrystal_I2C.h> //Download :: https://codeload.github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library/zip/master
-LiquidCrystal_I2C lcd(0x27,20,4);
+LiquidCrystal_I2C lcd(0x27,20,4); // Interface card (http://store.algobel.com) 
 
 void setup()
 {
   lcd.begin();   // initializing the LCD
   lcd.backlight(); // Enable or Turn On the backlight 
   lcd.setCursor(0, 0);
-  lcd.print("NodeMcu_Test"); // Printing Line
+  lcd.print("Original NodeMcu"); // Printing Line
+  lcd.setCursor(0, 1);
+  lcd.print("Algobel TI"); // Printing Line
   delay(1000);
 }
 
@@ -25,6 +34,8 @@ void loop()
 {
   lcd.clear();
   lcd.setCursor(0,0);  
-  lcd.print("Working Hehaaa.") ;
+  lcd.print("::BuY::") ;
+  lcd.setCursor(0, 1);
+  lcd.print("store.algobel.com"); // Printing Line
   delay(1000);
  }
